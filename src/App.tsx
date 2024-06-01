c82d458a59a1085605f61ce42f14e6bc40e0af46
katzper01 <katzper.michno@gmail.com>
2024-06-01 13:18:25 +0200

    rac shitpost

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import wrench from './wrench.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src={wrench} className="spinning-wrench" alt="logo" />
+        <div className="text1">
+          oops... there is nothing here
+        </div>
+        <div className="text2">
+          feel free to come back later
+        </div>
       </header>
     </div>
   );
